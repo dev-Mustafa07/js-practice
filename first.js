0c860bb2eef24c379bd5a8ad5bc8d5f6cf4ed34c
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-07-09 19:54:21 +0500

    Loop Basics

--- first.js
+++ first.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+// For loop
+for (let a  = 2; a <= 10; a++ ){
+    console.log(a)
+}
+
+// while loop
+let i = 11;
+
+while(i <= 20) {
+    console.log(i);
+    i++;
+}
+
+// Do-while loop
+let j = 21;
+
+do{
+    console.log(j);
+    j++;
+}while(j < 31)
