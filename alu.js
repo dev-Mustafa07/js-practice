4ce053e3354390546803ad4d8eda5067eb1c888c
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-07-11 20:02:49 +0500

    ALU proper program

--- alu.js
+++ alu.js
@@ -1,20 +1,7 @@
-let marks = 95;
+let a = 8;
+let b = 3;
 
-console.log("============================")
-console.log("   Marks Assessment System   ")
-console.log("============================")
-
-if (marks > 90 && marks <= 100){
-    console.log("Excellent")
-} else if (marks > 80) {
-    console.log("Very Good")
-} else if (marks > 70) {
-    console.log("Good")
-} else if (marks > 60) {
-    console.log("Pass")
-}
-else if (marks > 50) {
-    console.log("Fail")
-}else{
-    console.log("Please Enter Valid Numbers")
-}
+console.log(a-b)
+console.log(a+b)
+console.log(a/b)
+console.log(a*b)
